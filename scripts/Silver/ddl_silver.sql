46b988bf0a0493c6b83fa773bc83f3162e74dec5
Emmysilva9 <emmyblack9@outlook.com>
2026-01-04 12:58:41 +0400

    Update ddl_silver.sql

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -10,7 +10,8 @@ Run this script to re-defined the DDL structure of bronze tables .
 */
 If OBJECT_ID ( 'silver.crm_sales_detailes','U') is not Null
    Drop Table  silver.crm_sales_detailes
-Create Table  silver.crm_sales_detailes (
+go
+   Create Table  silver.crm_sales_detailes (
 Sls_Ord_num Nvarchar(50),
 Sls_Prd_key     Nvarchar(50),
 Sls_cust_ID      int,
@@ -20,10 +21,12 @@ sls_due_dt      int ,
 sls_sales       int,
 sls_quantity     int,
 sls_price       int
-)
+) go
 
   If OBJECT_ID ( 'silver.crm_cust_info','U') is not Null
-   Drop Table  silver.crm_cust_info
+   Drop Table  silver.crm_cust_info 
+Go
+   Create Table Silver.cust_info(
 cst_Id                   int,
 cst_key                  nvarchar(50),
 cst_firstname             nvarchar(50),
@@ -32,18 +35,46 @@ cst_marital_status        nvarchar  (50),
 cst_gndr                   nvarchar(50),
 cst_create_date            nvarchar(50),
 dwh_create_date            nvarchar(50)
-
-
-    If OBJECT_ID ( 'silver.crm_prd_info','U') is not Null
+)   go
+    
+   If OBJECT_ID ( 'silver.crm_prd_info','U') is not Null
   Drop Table  silver.crm_prd_info
-
-    create table Bronze.crm_prd_info (
+Go
+    create table silver.crm_prd_info (
     prd_ID                INT,
     prd_key               Nvarchar(50),
     prd_NM                Nvarchar(50),
     prd_cost              int,
     prd_line               nvarchar(50),
-    prd_start_dt          datetime,
-    prd_end_dt            datetime
-    ) 
+    prd_start_dt          date,
+    prd_end_dt            date
+    ) Go
+
+    
+    If OBJECT_ID ( 'silver.erp_cust_az12','U') is not Null
+  Drop Table  silver.erp_cust_az12
+Go
+    Create table silver.erp_cust_az12 (
+cid            nvarchar(50),
+bdate          date ,
+gen            nvarchar(50)
+) Go 
+
+   If OBJECT_ID ( ' silver.erp_loc_a101','U') is not Null
+  Drop Table  silver.erp_loc_a101
+Go
+    Create Table silver.erp_loc_a101 (
+    cid    nvarchar(50),
+    cntry   nvarchar(50)
+    ) Go 
   
+    If OBJECT_ID ( ' silver.erp_px_cat_giv12','U') is not Null
+  Drop Table silver.erp_px_cat_giv12
+go
+    Create table silver.erp_px_cat_giv12(
+    id                    nvarchar(50),
+    cat                   nvarchar(50),
+    subcat                nvarchar(50),
+    maintenace             nvarchar(50)
+    ) go
+
